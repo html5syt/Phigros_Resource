4b2aaa2024347da193e3737d5f3de8cd4a3c6174
Html5syt. <html5syt@163.com>
2025-08-07 22:45:29 +0800

    123

--- README.md
+++ README.md
@@ -1,21 +1,4 @@
-# Phigros_Resource
-本项目包含资源文件
+# PhigrosExtract
 
-资源包括
-
-[信息](../../tree/info)
-
-定数，收藏品id对应中文标题，头像id，tips
-
-曲id，曲名，曲师，画师，谱师
-
-[头像图片](../../tree/avatar)，
-[铺面文件](../../tree/chart)，
-[曲绘](../../tree/illustration)，
-[模糊曲绘](../../tree/illustrationBlur)，
-[低质量曲绘](../../tree/illustrationLowRes)，
-[音乐文件](../../tree/music)
-
-生成适配Phira的pez自制谱文件
-# 生成自制谱文件
-`python phira.py`
+# Thanks
+[yt6983138/DummyDllToPythonTemplate](https://github.com/yt6983138/DummyDllToPythonTemplate)
--- taptap.py
+++ taptap.py
@@ -1,10 +1,10 @@
 import hashlib
-from http.client import HTTPSConnection
 import json
 import random
 import string
 import time
 import urllib.parse
+import urllib.request
 import uuid
 
 sample = string.ascii_lowercase + string.digits
@@ -13,35 +13,33 @@ def taptap(appid):
     uid = uuid.uuid4()
     X_UA = "V=1&PN=TapTap&VN=2.40.1-rel.100000&VN_CODE=240011000&LOC=CN&LANG=zh_CN&CH=default&UID=%s&NT=1&SR=1080x2030&DEB=Xiaomi&DEM=Redmi+Note+5&OSV=9" % uid
     
-    conn = HTTPSConnection("api.taptapdada.com")
-    conn.request(
-        "GET",
-        "/app/v2/detail-by-id/%d?X-UA=%s" % (appid, urllib.parse.quote(X_UA)),
+    req = urllib.request.Request(
+        "https://api.taptapdada.com/app/v2/detail-by-id/%d?X-UA=%s" % (appid, urllib.parse.quote(X_UA)),
         headers={"User-Agent": "okhttp/3.12.1"}
     )
-    r = json.load(conn.getresponse())
-    print(r["data"]["download"])
+    with urllib.request.urlopen(req) as response:
+        r = json.load(response)
     apkid = r["data"]["download"]["apk_id"]
-
-    print("")
+    
 
     nonce = "".join(random.sample(sample, 5))
     t = int(time.time())
-    param = "abi=arm64-v8a,armeabi-v7a,armeabi&id=%d&node=%s&nonce=%s&sandbox=1&screen_densities=xhdpi&time=%s" % (apkid, uid, nonce, t)
-    byte = "X-UA=%s&%sPeCkE6Fu0B10Vm9BKfPfANwCUAn5POcs" % (X_UA, param)
+    byte = "X-UA=%s&abi=arm64-v8a,armeabi-v7a,armeabi&id=%d&node=%s&nonce=%s&sandbox=1&screen_densities=xhdpi&time=%sPeCkE6Fu0B10Vm9BKfPfANwCUAn5POcs" % (X_UA, apkid, uid, nonce, t)
     md5 = hashlib.md5(byte.encode()).hexdigest()
-    body = "%s&sign=%s" % (param, md5)
+    body = "node=%s&sandbox=1&sign=%s&abi=arm64-v8a%%2Carmeabi-v7a%%2Carmeabi&time=%s&id=%d&nonce=%s&screen_densities=xhdpi" % (uid, md5, t, apkid, nonce)
 
-    conn.request(
-        "POST",
-        "/apk/v1/detail?X-UA=" + urllib.parse.quote(X_UA),
-        body=body.encode(),
-        headers={"Content-Type": "application/x-www-form-urlencoded", "User-Agent": "okhttp/3.12.1"}
+    req = urllib.request.Request(
+        "https://api.taptapdada.com/apk/v1/detail?X-UA=" + urllib.parse.quote(X_UA),
+        body.encode(),
+        {"User-Agent": "okhttp/3.12.1"}
     )
-    r = json.load(conn.getresponse())
-    print(r)
+    with urllib.request.urlopen(req) as response:
+        return json.load(response)
+
 
 # Phigros app id = 165287
 if __name__ == "__main__":
-    taptap(165287)
-
+    r = taptap(165287)
+    with open("url.txt", "w") as f:
+        f.write(r["data"]["apk"]["download"])
+    print(r["data"]["apk"]["version_name"], end="")
